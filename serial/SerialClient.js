726e3db06899a9bc0e7ca697ef6ad0ebf4b992f6
장혁_집 <reaper83@naver.com>
2018-02-08 00:53:40 +0900

    edit serialClient

--- serial/SerialClient.js
+++ serial/SerialClient.js
@@ -1,25 +1,71 @@
 const serialport = require('serialport');
+
 const EventEmitter = require('events');
 const eventToPromise = require('event-to-promise');
 /** Class Serial Port 접속 클라이언트 클래스 */
 class SerialClient extends EventEmitter {
   /**
    * Serial Port 객체를 생성하기 위한 설정 정보
-   * @param {{port: string, baud_rate: number, target_name: string}} config {port, baud_rate, raget_name}
+   * @param {{port: string, baud_rate: number, target_name: string, parser: {type: string, option: *}}} config {port, baud_rate, raget_name}
    */
-  constructor(config = {
-    port,
-    baud_rate,
-    target_name
-  }) {
+  constructor(config) {
     super();
     this.client = {};
     this.port = config.port;
     this.baud_rate = config.baud_rate;
     this.target_name = config.target_name;
+    this.parser = config.parser;
+    this.hasInit = false;
   }
 
   _init(client) {
+    // BU.CLI('초기화 실행', this.parser)
+    if (this.parser !== undefined && this.parser.type !== undefined) {
+      this.hasInit = true;
+      let parser = null;
+      switch (this.parser.type) {
+        case 'delimiterParser':
+          // BU.CLI('delimiterParser', this.parser)
+          const Delimiter = serialport.parsers.Delimiter;
+          parser = client.pipe(new Delimiter({
+            delimiter: this.parser.option
+          }));
+          parser.on('data', data => {
+            this._onData(Buffer.concat([data, this.parser.option]))
+          })
+          break;
+        case 'byteLengthParser':
+          const ByteLength = serialport.parsers.ByteLength;
+          parser = client.pipe(new ByteLength({
+            length: this.parser.option
+          }));
+          parser.on('data', data => {
+            this._onData(data)
+          })
+          break;
+        case 'readLineParser':
+          const Readline = serialport.parsers.Readline;
+          parser = client.pipe(new Readline({
+            delimiter: this.parser.option
+          }));
+          parser.on('data', data => {
+            this._onData(data)
+          })
+          break;
+        case 'readyParser':
+          const Ready = serialport.parsers.Ready;
+          parser = client.pipe(new Ready({
+            delimiter: this.parser.option
+          }));
+          parser.on('data', data => {
+            this._onData(data)
+          })
+          break;
+        default:
+          break;
+      }
+
+    }
   }
 
   _onData(bufferData) {
@@ -39,8 +85,12 @@ class SerialClient extends EventEmitter {
       baudRate: this.baud_rate,
     });
 
-    this.client.on('data',  bufferData => {
-      this._onData(bufferData);
+    this._init(this.client)
+
+    this.client.on('data', bufferData => {
+      if (!this.hasInit) {
+        this._onData(bufferData);
+      }
     });
 
     this.client.on('close', err => {
